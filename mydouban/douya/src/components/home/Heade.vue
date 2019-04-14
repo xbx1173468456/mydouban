5c01553f273e96d85d74fc9ba96785ab0dc47df4
xbx1173468456 <1173468456@qq.com>
2019-04-15 07:27:54 +0800

    4.15am

---
+++ mydouban/douya/src/components/home/Heade.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="app-home">
+    <mt-header fixed title="Move"></mt-header>
+    <input class="home-inp" type="text" placeholder="请输入搜索内容">
+    <mt-navbar>
+      <mt-tab-item id="h1">正在热映</mt-tab-item>
+      <mt-tab-item id="h1">即将上映</mt-tab-item>
+      <mt-tab-item id="h1">Top25</mt-tab-item>
+    </mt-navbar>
+    <mt-tab-container>
+      <mt-tab-container-item>
+        <mt-cell></mt-cell>
+      </mt-tab-container-item>
+    </mt-tab-container>
+  </div>
+</template>
+<script>
+export default {
+   data(){
+     return {
+    value:''
+     }
+   },
+  methods:{
+    app(){
+      var url="api/movie/in_theaters";
+      this.axios.get(url).then(result=>{
+        console.log(result);
+      });
+    }
+  },
+  created(){
+    this.app();
+  }
+}
+</script>
+<style>
+input.home-inp{
+  margin-bottom: 0px;
+}
+</style>
--- mydouban/douya/src/main.js
+++ mydouban/douya/src/main.js
@@ -1,19 +1,61 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-import axios from 'axios'
-
-axios.defaults.withCrendtials=true;
-Vue.prototype.axios=axios;
+import App from './App.vue'
+import router from './router/index.js'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+import'mint-ui/lib/style.css'
+import './lib/mui/css/mui.css'
+// 导入 MUI 的样式表，扩展图标样式，购物车图标
+// 还需要加载图标字体文件
+import './lib/mui/css/icons-extra.css'
+
+
+//1:引入Header组件
+import {Header,Search,Navbar,TabItem} from "mint-ui"
+//2:组件Header组件
+Vue.component(Header.name,Header);
+Vue.component(Search.name,Search);
+Vue.component(Navbar.name, Navbar);
+Vue.component(TabItem.name, TabItem);
+
+//3:引入axios库
+import axios from "axios"
+//4:配置跨域访问选项
+axios.defaults.withCredentials=true
+//5:将axios配置Vue实例属性
+Vue.prototype.axios = axios
+
+//6:创建日期格式过滤器 main.js
+//val 原始日期格式
+Vue.filter("datetimeFilter",function(val){
+   //6.1:依据原始日期格式创建日期对象
+   var now = new Date(val);
+   //6.2:获取日期对象 年 月 日
+   var y = now.getFullYear();
+   var m = now.getMonth()+1;
+   var d = now.getDate(); 
+   //6.3:获取日期对象 时 分 秒
+   var h = now.getHours();
+   var mi = now.getMinutes();
+   var s = now.getSeconds();
+   //6.4:月份与日期 如果当前月分小于10前拼接0
+   m<10&&(m="0"+m);
+   d<10&&(d="0"+d);
+   //6.5:拼接字符串返回
+   return `${y}-${m}-${d} ${h}:${mi}:${s}`;
+});
+//7:main.js 引入第三方库 qs
+//解决axios post请求问题 
+import qs from "qs";
+Vue.prototype.qs = qs;
+
+
+
+
+
+
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app')
